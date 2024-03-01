5030312eb8c2ddd9245e5f7c80eeb61a77942c73
Stephen Chung <spchung95@gmail.com>
2024-03-01 13:00:36 -0500

    note

--- ch7/7.5/limitreader/reader.go
+++ ch7/7.5/limitreader/reader.go
@@ -13,6 +13,13 @@ func LimitReader(r io.Reader, n int) io.Reader {
 	return &Reader{r, n}
 }
 
+// this was confusing...
+// but i think the idea here is:
+// let the underlying reader read however many bytes into p
+// if p is larger than how much we can continue to read (Reader.remaining),
+// then we make sure to only allow reading by at most Reader.remaining bytes.
+// at the end, we still return how much the internal reader read, along with
+// any error
 func (r *Reader) Read(p []byte) (int, error) {
 	if r.remaining <= 0 {
 		return 0, io.EOF
